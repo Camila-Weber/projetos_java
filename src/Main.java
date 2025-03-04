851fd7442a9ce5457343e4d60e4e541e6e44a4ca
Camila Weber <camilawe2004@gmail.com>
2025-03-04 18:05:45 -0400

    operações e formatação de texto

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,18 @@ public class Main {
         boolean incluidoNoPlano = true;
         double notaDoFilme = 5.9;
 
+        double media = (9.8 + 6.3 + 8 + notaDoFilme) / 4;
+        System.out.println("Média: " + media);
+
+        String sinopse = "Filme de aventura com galã dos anos 80.";
+        System.out.println(sinopse);
+        // formatação de texto com concatenação de variáveis com o printf
+        System.out.printf("""
+                Filme: Top Gun
+                Sinopse: %s
+                Ano de lançamento: %d
+                Média de notas: %.2f
+                """, sinopse, anoDeLancamento, media);
+
     }
 }
