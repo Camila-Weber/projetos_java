0979de2b84624c2766f3475ebd85d4b63b3f0b44
Camila Weber <camilawe2004@gmail.com>
2025-03-04 19:03:39 -0400

    loop for

---
+++ src/Loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite a sua avaliação:");
+            nota = leitura.nextDouble();
+            leitura.nextLine();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.println("A média das avaliações é " + (mediaAvaliacao / 5));
+    }
+}
