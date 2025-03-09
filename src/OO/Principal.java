a30029b1d0c90ba26ef5172b7ac6eb8c53caf78e
Camila Weber <camilawe2004@gmail.com>
2025-03-09 17:02:59 -0400

    herança

--- src/OO/Principal.java
+++ src/OO/Principal.java
@@ -1,11 +1,15 @@
 package OO;
 
+import OO.screenmatch.modelo.Filme;
+import OO.screenmatch.modelo.Serie;
+
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
 
-        filme1.nome = "Barbie";
-        filme1.anoDeLancamento = 2001;
+        filme1.setNome("Barbie");
+        filme1.setAnoDeLancamento(2001);
+        filme1.setDuracaoEmMinutos(95);
 
         filme1.avalia(8.9);
         filme1.avalia(6.7);
@@ -13,5 +17,13 @@ public class Principal {
 
         filme1.exibeFichaTecnica();
         System.out.println("Média: " + filme1.retornaMedia());
+        System.out.println("Total de avaliações: " + filme1.getTotalAvaliacoes());
+
+        Serie serie1 = new Serie();
+        serie1.setNome("Lost");
+        serie1.setTemporadas(10);
+        serie1.setEpisodiosPorTemporada(10);
+        serie1.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar " + serie1.getNome() + ": " + serie1.getDuracaoEmMinutos());
     }
 }
---
+++ src/OO/screenmatch/modelo/Titulo.java
@@ -0,0 +1,59 @@
+package OO.screenmatch.modelo;
+
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
+
+    // Getters e Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalAvaliacoes() {
+        return totalAvaliacoes;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    // Métodos
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double retornaMedia(){
+        return somaAvaliacoes / totalAvaliacoes;
+    }
+
+}
